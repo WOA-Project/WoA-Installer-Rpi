dc6d1091883b63b068444f48ecb1c89f802ff322
mrmajik45 <35823544+mrmajik45@users.noreply.github.com>
2018-09-20 15:20:43 -0400

    An closing/opening curly bracket must not be preceded/followed by a blank line.  (lines of code = 1)

--- Installer.Core/Exceptions/PhoneDiskNotFoundException.cs
+++ Installer.Core/Exceptions/PhoneDiskNotFoundException.cs
@@ -5,8 +5,5 @@ namespace Installer.Core.Exceptions
     public class PhoneDiskNotFoundException : Exception
     {
         public PhoneDiskNotFoundException(string message)  : base(message)
-        {
-            
-        }
     }
 }
